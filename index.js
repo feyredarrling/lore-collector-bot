525d297f4bbea34653b82e136f5d28e17d59fd0e
Sarah Darr <Feyre Darrling>
2026-05-11 09:11:22 -0400

    fixing enchanted message

--- index.js
+++ index.js
@@ -1293,20 +1293,6 @@ if (randomCard.rarity === 'Enchanted') {
   );
 
 }
-
-      if (randomCard.rarity === 'Enchanted') {
-
-  await interaction.followUp(
-    `🚨 ${interaction.user.username} pulled an ENCHANTED daily card!\n🌈 **ENCHANTED:** ${randomCard.name}`
-  );
-
-} else if (randomCard.rarity === 'Legendary') {
-
-  await interaction.followUp(
-    `🚨 ${interaction.user.username} pulled a LEGENDARY daily card!\n💎 **LEGENDARY:** ${randomCard.name}`
-  );
-
-}
     }
 
     if (interaction.commandName === 'balance') {
